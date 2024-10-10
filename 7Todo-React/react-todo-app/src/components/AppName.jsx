350d5e1242e08fd9ef41c7ad18067d5ea2f9d0b5
jaigoel08 <jaigoel14@gmail.com>
2024-10-10 23:55:29 +0530

    new commits

--- 7Todo-React/react-todo-app/src/components/AppName.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AppName = () => {
-  return (
-   <h1 className= "text-center text-3xl text-blue-950 font-bold">TODO-APP</h1>
-  )
-}
-
-export default AppName
--- todo-app/my-project/src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Heading = () => {
-  return (
-    <h1 className="text-3xl font-bold mb-6 text-center">To-Do App</h1>
-  )
-}
-
-export default Heading
